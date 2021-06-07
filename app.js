43c19ed1d7571c77c9489cf17907b6af96e6b587
chloe <itprofessional.4ever7@gmail.com>
2021-06-07 14:19:19 -0600

    commit todoList v1.0

--- app.js
+++ app.js
@@ -1,47 +1,45 @@
+//jshint esversion:6
+
 const express = require("express")
 const bodyParser = require("body-parser")
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
+const items = ["Data Structure", "HTML/CSS/JavaScript", "Searching for Job Posting"];
+const workItems = [];
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
-
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to: " + currentDay);
-
-    }
+    const day = date.getDate();
 
     res.render("list", {
-        kindOfDay: day
+        listTitle: day, newListItems: items
     });
 
+});
+
+app.post("/", function (req, res) {
+    const item = req.body.newItem;
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work");
+    } else {
+        items.push(item);
+        res.redirect("/");
+    }
+})
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
+    res.render("about")
 });
 
 app.listen(3000, function () {
